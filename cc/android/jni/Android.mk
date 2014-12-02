0d58cefbe8790f33c0743daa0d519231b2f47483
Brent Austin <baustin@google.com>
2014-12-02 03:11:11 -0800

    Compile dependencies from libcutils and libutils.
    Switched to SDK version 21.
    Added/modified some compiler options.
    
    Change-Id: I7c4a7890c747005ae8120bb2a10342e1df9718e0

--- cc/android/jni/Android.mk
+++ cc/android/jni/Android.mk
@@ -3,24 +3,67 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 # Use the pre-built protobuf library.
-SRC_FILE_LIST          := $(LOCAL_PATH)/../../../../../external/protobuf/prebuilts/arm/libprotobuf-cpp-2.3.0-lite.a
+SRC_FILE_LIST          := $(LOCAL_PATH)/../../../../../external/protobuf/prebuilts/$(TARGET_ARCH)/libprotobuf-cpp-2.3.0-lite.a
 LOCAL_MODULE           := libprotobuf
 LOCAL_SRC_FILES        := $(SRC_FILE_LIST:$(LOCAL_PATH)/%=%)
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
-# TODO: Change this to *.cpp once all the files are ready to compile.
-SRC_FILE_LIST          := $(wildcard $(LOCAL_PATH)/../../src/gfxspy/gltrace.pb.cpp)
+# Define ANDROID_SMP appropriately (used by atomic.c).
+ifeq ($(TARGET_CPU_SMP),true)
+    libc_common_cflags += -DANDROID_SMP=1
+else
+    libc_common_cflags += -DANDROID_SMP=0
+endif
 
+SRC_FILE_LIST          := \
+    $(wildcard $(LOCAL_PATH)/../../src/gfxspy/libcutils/*.c) \
+    $(wildcard $(LOCAL_PATH)/../../src/gfxspy/libutils/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/../../src/gfxspy/gltrace.pb.cpp)
 LOCAL_MODULE           := gfxspy
 LOCAL_SRC_FILES        := $(SRC_FILE_LIST:$(LOCAL_PATH)/%=%)
-
-LOCAL_CPPFLAGS         := -DEGL_EGLEXT_PROTOTYPES -DGL_GLEXT_PROTOTYPES -DTARGET_OS_ANDROID -DANDROID_NDK -Ofast -DGOOGLE_PROTOBUF_NO_RTTI -DHAVE_SYS_UIO_H -DHAVE_PTHREADS -DGENERATE_API_WRAPPERS -DLOAD_HOOKS_AT_STARTUP -O0 -marm -fno-strict-aliasing -funswitch-loops -finline-limit=100 -fno-omit-frame-pointer -ffunction-sections -mfloat-abi=softfp -fpic -fstack-protector -funwind-tables -fno-short-enums -fno-exceptions
-
-LOCAL_C_INCLUDES       := /usr/local/include $(LOCAL_PATH)/../../../../../external/protobuf/src
+LOCAL_CXXFLAGS         := \
+    $(libc_common_cflags) \
+    -DEGL_EGLEXT_PROTOTYPES \
+    -DGL_GLEXT_PROTOTYPES \
+    -DTARGET_OS_ANDROID \
+    -DANDROID_NDK \
+    -DGOOGLE_PROTOBUF_NO_RTTI \
+    -DHAVE_SYS_UIO_H \
+    -DHAVE_PTHREADS \
+    -DGENERATE_API_WRAPPERS \
+    -DLOAD_HOOKS_AT_STARTUP \
+    -D__NDK_FPABI_MATH__= \
+    -DOS_PATH_SEPARATOR=\'/\' \
+    -Ofast \
+    -O0 \
+    -marm \
+    -mfloat-abi=softfp \
+    -fno-strict-aliasing \
+    -funswitch-loops \
+    -finline-limit=100 \
+    -fno-omit-frame-pointer \
+    -ffunction-sections \
+    -fpic \
+    -fstack-protector \
+    -funwind-tables \
+    -fno-short-enums \
+    -fno-exceptions
+LOCAL_CFLAGS         := \
+    $(libc_common_cflags)
+LOCAL_C_INCLUDES       := \
+    $(LOCAL_PATH)/../../src/gfxspy/ \
+    $(LOCAL_PATH)/../../../../../frameworks/native/opengl/include \
+    $(LOCAL_PATH)/../../../../../frameworks/native/opengl/libs \
+    $(LOCAL_PATH)/../../../../../external/protobuf/src \
+    $(LOCAL_PATH)/../../../../../system/core/include \
+    $(LOCAL_PATH)/../../../../../bionic/libc/include \
+    $(LOCAL_PATH)/../../../../../bionic/libc/private \
+    /usr/local/include
 LOCAL_LDLIBS           := -lc -lm -llog
 LOCAL_STATIC_LIBRARIES := libprotobuf libstlport_static
 LOCAL_SHARED_LIBRARIES := -lgcc
 
 include $(BUILD_SHARED_LIBRARY)
+
--- cc/android/jni/Application.mk
+++ cc/android/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := armeabi-v7a
 APP_CPPFLAGS += -std=c++11
-APP_PLATFORM := android-19
+APP_PLATFORM := android-21
 APP_STL := stlport_static
 NDK_TOOLCHAIN_VERSION := clang
 APP_OPTIM := debug
