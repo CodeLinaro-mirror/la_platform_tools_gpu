a46e4d00dfbe039b3f44c4d4dc6d789b47ca6b51
Ben Clayton <bclayton@google.com>
2015-02-20 23:26:00 +0000

    Initial drop of the replay/asm package.
    Package asm contains high-level instructions to control the replay VM.
    
    Change-Id: I561327d199844ab98cf1ddc013185cfabed4a371

---
+++ replay/asm/instructions.go
@@ -0,0 +1,308 @@
+/*
+ * Copyright 2015, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package asm
+
+import (
+	"fmt"
+	"gaze/memory"
+
+	"android.googlesource.com/platform/tools/gpu/binary"
+	"android.googlesource.com/platform/tools/gpu/replay/opcode"
+	"android.googlesource.com/platform/tools/gpu/replay/value"
+	"android.googlesource.com/platform/tools/gpu/replay/vm"
+)
+
+// Instruction is the interface of all instruction types.
+//
+// Encode writes the instruction's opcodes to the binary encoder e, translating
+// any pointers to their final, resolved addresses using the PointerResolver r.
+// An instruction can produce zero, one or many opcodes.
+type Instruction interface {
+	Encode(r value.PointerResolver, e *binary.Encoder) error
+}
+
+func encodePush(t vm.Type, v uint64, e *binary.Encoder) error {
+	mask19 := uint64(0x7ffff)
+	mask20 := uint64(0xfffff)
+	mask26 := uint64(0x3ffffff)
+	mask45 := uint64(0x1fffffffffff)
+	mask46 := uint64(0x3fffffffffff)
+
+	//     ▏60       ▏50       ▏40       ▏30       ▏20       ▏10
+	// ○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○●●●●●●●●●●●●●●●●●●● mask19
+	// ○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○●●●●●●●●●●●●●●●●●●●● mask20
+	// ○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○●●●●●●●●●●●●●●●●●●●●●●●●●● mask26
+	// ○○○○○○○○○○○○○○○○○○○●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● mask45
+	// ○○○○○○○○○○○○○○○○○○●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● mask46
+	//                                            ▕      PUSHI 20     ▕
+	//                                      ▕         EXTEND 26       ▕
+
+	switch t {
+	case vm.TypeFloat:
+		push := opcode.PushI{DataType: t, Value: uint32(v >> 23)}
+		if err := push.Encode(e); err != nil {
+			return err
+		}
+		if v&0x7fffff != 0 {
+			return opcode.Extend{Value: uint32(v & 0x7fffff)}.Encode(e)
+		}
+	case vm.TypeDouble:
+		push := opcode.PushI{DataType: t, Value: uint32(v >> 52)}
+		if err := push.Encode(e); err != nil {
+			return err
+		}
+		if v&0xfffffffffffff != 0 {
+			ext := opcode.Extend{Value: uint32(v >> 26)}
+			if err := ext.Encode(e); err != nil {
+				return err
+			}
+			return opcode.Extend{Value: uint32(v)}.Encode(e)
+		}
+	case vm.TypeInt8, vm.TypeInt16, vm.TypeInt32, vm.TypeInt64:
+		// Signed PUSHI types are sign-extended
+		switch {
+		case v&^mask19 == 0:
+			// ○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒
+			//                                            ▕      PUSHI 20     ▕
+			return opcode.PushI{DataType: t, Value: uint32(v)}.Encode(e)
+		case v&^mask19 == ^mask19:
+			// ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒
+			//                                            ▕      PUSHI 20     ▕
+			return opcode.PushI{DataType: t, Value: uint32(v & mask20)}.Encode(e)
+		case v&^mask45 == 0:
+			// ○○○○○○○○○○○○○○○○○○○◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒
+			//                  ▕      PUSHI 20     ▕         EXTEND 26       ▕
+			push := opcode.PushI{DataType: t, Value: uint32(v >> 26)}
+			if err := push.Encode(e); err != nil {
+				return err
+			}
+			return opcode.Extend{uint32(v & mask26)}.Encode(e)
+		case v&^mask45 == ^mask45:
+			// ●●●●●●●●●●●●●●●●●●●◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒
+			//                  ▕      PUSHI 20     ▕         EXTEND 26       ▕
+			push := opcode.PushI{DataType: t, Value: uint32((v >> 26) & mask20)}
+			if err := push.Encode(e); err != nil {
+				return err
+			}
+			return opcode.Extend{uint32(v & mask26)}.Encode(e)
+		default:
+			// ◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒
+			//▕  PUSHI 12 ▕         EXTEND 26       ▕         EXTEND 26       ▕
+			push := opcode.PushI{DataType: t, Value: uint32(v >> 52)}
+			if err := push.Encode(e); err != nil {
+				return err
+			}
+			ext := opcode.Extend{uint32((v >> 26) & mask26)}
+			if err := ext.Encode(e); err != nil {
+				return err
+			}
+			return opcode.Extend{uint32(v & mask26)}.Encode(e)
+		}
+	case vm.TypeBool,
+		vm.TypeUint8, vm.TypeUint16, vm.TypeUint32, vm.TypeUint64,
+		vm.TypeAbsolutePointer, vm.TypeConstantPointer, vm.TypeVolatilePointer:
+		switch {
+		case v&^mask20 == 0:
+			// ○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒
+			//                                            ▕      PUSHI 20     ▕
+			return opcode.PushI{DataType: t, Value: uint32(v)}.Encode(e)
+		case v&^mask46 == 0:
+			// ○○○○○○○○○○○○○○○○○○◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒
+			//                  ▕      PUSHI 20     ▕         EXTEND 26       ▕
+			push := opcode.PushI{DataType: t, Value: uint32(v >> 26)}
+			if err := push.Encode(e); err != nil {
+				return err
+			}
+			return opcode.Extend{uint32(v & mask26)}.Encode(e)
+		default:
+			// ◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒◒
+			//▕  PUSHI 12 ▕         EXTEND 26       ▕         EXTEND 26       ▕
+			push := opcode.PushI{DataType: t, Value: uint32(v >> 52)}
+			if err := push.Encode(e); err != nil {
+				return err
+			}
+			ext := opcode.Extend{uint32((v >> 26) & mask26)}
+			if err := ext.Encode(e); err != nil {
+				return err
+			}
+			return opcode.Extend{uint32(v & mask26)}.Encode(e)
+		}
+	}
+	return fmt.Errorf("Cannot push value type %s", t.String())
+}
+
+// Nop is a no-operation Instruction. Instructions of this type do nothing.
+type Nop struct{}
+
+func (Nop) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	return nil
+}
+
+// Call is an Instruction to call a VM registered function.
+// This instruction will pop the parameters from the  VM stack starting with the
+// first parameter. If PushReturn is true, then the return value of the function
+// call will be pushed to the top of the VM stack.
+type Call struct {
+	PushReturn bool   // If true, the return value is pushed to the VM stack.
+	FunctionId uint16 // The function id registered with the VM to invoke.
+}
+
+func (a Call) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	return opcode.Call{
+		PushReturn: a.PushReturn,
+		FunctionId: a.FunctionId,
+	}.Encode(e)
+}
+
+// Push is an Instruction to push Value to the top of the VM stack.
+type Push struct {
+	Value value.Value // The value to push on to the VM stack.
+}
+
+func (a Push) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	return encodePush(a.Value.Type(), a.Value.Get(r), e)
+}
+
+// Pop is an Instruction that discards Count values from the top of the VM
+// stack.
+type Pop struct {
+	Count uint32 // Number of values to discard from the top of the VM stack.
+}
+
+func (a Pop) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	return opcode.Pop{Count: a.Count}.Encode(e)
+}
+
+// Copy is an Instruction that pops the source address and then the target
+// address from the top of the VM stack, and then copies Count bytes from
+// source to target.
+type Copy struct {
+	Count uint64 // Number of bytes to copy.
+}
+
+func (a Copy) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	return opcode.Copy{Count: uint32(a.Count)}.Encode(e)
+}
+
+// Clone is an Instruction that pushes a copy of the the n-th element from the
+// top of the VM stack to the top of the VM stack.
+type Clone struct {
+	Index int
+}
+
+func (a Clone) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	return opcode.Clone{Index: uint32(a.Index)}.Encode(e)
+}
+
+// Load is an Instruction that loads the value of type DataType from pointer
+// Source and pushes the loaded value to the top of the VM stack.
+type Load struct {
+	DataType vm.Type
+	Source   value.Pointer
+}
+
+func (a Load) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	addr := a.Source.Get(r)
+	switch a.Source.(type) {
+	case value.ConstantPointer:
+		if addr < 0x100000 {
+			return opcode.LoadC{DataType: a.DataType, Address: uint32(addr)}.Encode(e)
+		} else {
+			if err := encodePush(a.Source.Type(), a.Source.Get(r), e); err != nil {
+				return err
+			}
+			return opcode.Load{DataType: a.DataType}.Encode(e)
+		}
+	case value.VolatilePointer, value.VolatileTemporaryPointer, value.VolatileCapturePointer:
+		if addr < 0x100000 {
+			return opcode.LoadV{DataType: a.DataType, Address: uint32(addr)}.Encode(e)
+		} else {
+			if err := encodePush(a.Source.Type(), a.Source.Get(r), e); err != nil {
+				return err
+			}
+			return opcode.Load{DataType: a.DataType}.Encode(e)
+		}
+	default:
+		return fmt.Errorf("Unsupported load source type %T", a.Source)
+	}
+}
+
+// Store is an Instruction that pops the value from the top of the VM stack and
+// writes the value to Destination.
+type Store struct {
+	Destination value.Pointer
+}
+
+func (a Store) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	addr := a.Destination.Get(r)
+	if addr < 0x3ffffff {
+		return opcode.StoreV{Address: uint32(addr)}.Encode(e)
+	} else {
+		if err := encodePush(a.Destination.Type(), a.Destination.Get(r), e); err != nil {
+			return err
+		}
+		return opcode.Store{}.Encode(e)
+	}
+}
+
+// Strcpy is an Instruction that pops the source address then the target address
+// from the top of the VM stack, and then copies at most MaxCount-1 bytes from
+// source to target. If the MaxCount is greater than the source string length,
+// then the target will be padded with 0s. The destination buffer will always be
+// 0-terminated.
+type Strcpy struct {
+	MaxCount uint64
+}
+
+func (a Strcpy) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	return opcode.Strcpy{
+		MaxSize: uint32(a.MaxCount),
+	}.Encode(e)
+}
+
+// Resource is an Instruction that loads the resource with index Index of Size
+// bytes and writes the resource to Destination.
+type Resource struct {
+	Index       uint32
+	Destination memory.Pointer
+}
+
+func (a Resource) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	ptr := value.VolatileCapturePointer(a.Destination)
+	if err := encodePush(ptr.Type(), ptr.Get(r), e); err != nil {
+		return err
+	}
+	return opcode.Resource{
+		Id: a.Index,
+	}.Encode(e)
+}
+
+// Post is an Instruction that posts Size bytes from Source to the server.
+type Post struct {
+	Source value.Pointer
+	Size   uint64
+}
+
+func (a Post) Encode(r value.PointerResolver, e *binary.Encoder) error {
+	if err := encodePush(a.Source.Type(), a.Source.Get(r), e); err != nil {
+		return err
+	}
+	if err := encodePush(vm.TypeUint32, a.Size, e); err != nil {
+		return err
+	}
+	return opcode.Post{}.Encode(e)
+}
---
+++ replay/asm/instructions_test.go
@@ -0,0 +1,307 @@
+/*
+ * Copyright 2015, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package asm
+
+import (
+	"bytes"
+	"testing"
+
+	"android.googlesource.com/platform/tools/gpu/binary"
+	"android.googlesource.com/platform/tools/gpu/replay/opcode"
+	"android.googlesource.com/platform/tools/gpu/replay/value"
+	"android.googlesource.com/platform/tools/gpu/replay/vm"
+)
+
+type testPtrResolver struct{}
+
+func (testPtrResolver) TranslateTemporaryPointer(ptr uint64) uint64 { return ptr }
+func (testPtrResolver) TranslateCapturePointer(ptr uint64) uint64   { return ptr }
+
+func check(t *testing.T, Instructions []Instruction, expected ...interface{}) {
+	buf := &bytes.Buffer{}
+	b := binary.NewEncoder(buf)
+	for _, Instruction := range Instructions {
+		Instruction.Encode(testPtrResolver{}, b)
+	}
+	gotOpcodes, err := opcode.Disassemble(buf)
+	if err != nil {
+		panic(err)
+	}
+	opcode.CheckDisassembly(t, gotOpcodes, expected...)
+}
+
+func TestCall(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Call{false, 0x1234},
+			Call{true, 0x5678},
+		},
+		opcode.Call{PushReturn: false, FunctionId: 0x1234},
+		opcode.Call{PushReturn: true, FunctionId: 0x5678},
+	)
+}
+
+func TestPush_UnsignedNoExpand(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Push{value.U32(0xaaaaa)}, // Repeating pattern of 1010
+			Push{value.U32(0x55555)}, // Repeating pattern of 0101
+		},
+		opcode.PushI{DataType: vm.TypeUint32, Value: 0xaaaaa},
+		opcode.PushI{DataType: vm.TypeUint32, Value: 0x55555},
+	)
+}
+
+func TestPush_UnsignedOneExpand(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Push{value.U32(0x100000)},   // One bit beyond what can fit in a PushI
+			Push{value.U32(0x4000000)},  // One bit beyond what can fit in a Extend payload
+			Push{value.U32(0xaaaaaaaa)}, // 1010101010...
+			Push{value.U32(0x55555555)}, // 0101010101...
+		},
+		opcode.PushI{DataType: vm.TypeUint32, Value: 0},
+		opcode.Extend{Value: 0x100000},
+
+		opcode.PushI{DataType: vm.TypeUint32, Value: 1},
+		opcode.Extend{Value: 0},
+
+		opcode.PushI{DataType: vm.TypeUint32, Value: 0x2a},
+		opcode.Extend{Value: 0x2aaaaaa},
+
+		opcode.PushI{DataType: vm.TypeUint32, Value: 0x15},
+		opcode.Extend{Value: 0x1555555},
+	)
+}
+
+func TestPush_SignedPositiveNoExpand(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Push{value.S32(0x2aaaa)}, // 0010101010...
+			Push{value.S32(0x55555)}, // 1010101010...
+		},
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0x2aaaa},
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0x55555},
+	)
+}
+
+func TestPush_SignedPositiveOneExpand(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Push{value.S32(0x80000)},    // One bit beyond what can fit in a PushI
+			Push{value.S32(0x4000000)},  // One bit beyond what can fit in a Extend payload
+			Push{value.S32(0x2aaaaaaa)}, // 0010101010...
+			Push{value.S32(0x55555555)}, // 0101010101...
+		},
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0},
+		opcode.Extend{Value: 0x80000},
+
+		opcode.PushI{DataType: vm.TypeInt32, Value: 1},
+		opcode.Extend{Value: 0},
+
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0x0a},
+		opcode.Extend{Value: 0x2aaaaaa},
+
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0x15},
+		opcode.Extend{Value: 0x1555555},
+	)
+}
+
+func TestPush_SignedNegativeNoExpand(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Push{value.S32(-1)},
+			Push{value.S32(-0x55556)}, // Repeating pattern of 1010
+			Push{value.S32(-0x2aaab)}, // Repeating pattern of 0101
+		},
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0xfffff},
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0xaaaaa},
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0xd5555},
+	)
+}
+
+func TestPush_SignedNegativeOneExpand(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Push{value.S32(-0x100001)},  // One bit beyond what can fit in a PushI
+			Push{value.S32(-0x4000001)}, // One bit beyond what can fit in a Extend payload
+
+			Push{value.S32(-0x2aaaaaab)}, // 110101010...
+			Push{value.S32(-0x55555556)}, // 101010101...
+		},
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0xfffff},
+		opcode.Extend{Value: 0x03efffff},
+
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0xffffe},
+		opcode.Extend{Value: 0x03ffffff},
+
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0xffff5},
+		opcode.Extend{Value: 0x1555555},
+
+		opcode.PushI{DataType: vm.TypeInt32, Value: 0xfffea},
+		opcode.Extend{Value: 0x2aaaaaa},
+	)
+}
+
+func TestPush_FloatNoExpand(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Push{value.F32(-2.0)},
+			Push{value.F32(-1.0)},
+			Push{value.F32(-0.5)},
+			Push{value.F32(0)},
+			Push{value.F32(0.5)},
+			Push{value.F32(1.0)},
+			Push{value.F32(2.0)},
+		},
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x180},
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x17f},
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x17e},
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x000},
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x07e},
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x07f},
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x080},
+	)
+}
+
+func TestPush_FloatOneExpand(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Push{value.F32(-3)},
+			Push{value.F32(-1.75)},
+			Push{value.F32(1.75)},
+			Push{value.F32(3)},
+		},
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x180},
+		opcode.Extend{Value: 0x400000},
+
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x17F},
+		opcode.Extend{Value: 0x600000},
+
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x07F},
+		opcode.Extend{Value: 0x600000},
+
+		opcode.PushI{DataType: vm.TypeFloat, Value: 0x080},
+		opcode.Extend{Value: 0x400000},
+	)
+}
+
+func TestPop(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Pop{10},
+		},
+		opcode.Pop{Count: 10},
+	)
+}
+
+func TestCopy(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Copy{100},
+		},
+		opcode.Copy{Count: 100},
+	)
+}
+
+func TestClone(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Clone{100},
+		},
+		opcode.Clone{Index: 100},
+	)
+}
+
+func TestLoad(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Load{vm.TypeUint16, value.ConstantPointer(0x10)},
+			Load{vm.TypeUint16, value.ConstantPointer(0x123456)},
+
+			Load{vm.TypeUint16, value.VolatilePointer(0x10)},
+			Load{vm.TypeUint16, value.VolatilePointer(0x123456)},
+		},
+		opcode.LoadC{DataType: vm.TypeUint16, Address: 0x10},
+
+		opcode.PushI{DataType: vm.TypeConstantPointer, Value: 0},
+		opcode.Extend{Value: 0x123456},
+		opcode.Load{DataType: vm.TypeUint16},
+
+		opcode.LoadV{DataType: vm.TypeUint16, Address: 0x10},
+
+		opcode.PushI{DataType: vm.TypeVolatilePointer, Value: 0},
+		opcode.Extend{Value: 0x123456},
+		opcode.Load{DataType: vm.TypeUint16},
+	)
+}
+
+func TestStore(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Store{value.VolatilePointer(0x10)},
+			Store{value.VolatilePointer(0x4000000)},
+		},
+		opcode.StoreV{Address: 0x10},
+
+		opcode.PushI{DataType: vm.TypeVolatilePointer, Value: 1},
+		opcode.Extend{Value: 0},
+		opcode.Store{},
+	)
+}
+
+func TestStrcpy(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Strcpy{0x3000},
+		},
+		opcode.Strcpy{MaxSize: 0x3000},
+	)
+}
+
+func TestResource(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Resource{10, 0x10},
+			Resource{20, 0x4050607},
+		},
+		opcode.PushI{DataType: vm.TypeVolatilePointer, Value: 0x10},
+		opcode.Resource{10},
+
+		opcode.PushI{DataType: vm.TypeVolatilePointer, Value: 1},
+		opcode.Extend{Value: 0x50607},
+		opcode.Resource{20},
+	)
+}
+
+func TestPost(t *testing.T) {
+	check(t,
+		[]Instruction{
+			Post{value.AbsolutePointer(0x10), 0x50},
+			Post{value.VolatilePointer(0x4050607), 0x8090a0b},
+		},
+		opcode.PushI{DataType: vm.TypeAbsolutePointer, Value: 0x10},
+		opcode.PushI{DataType: vm.TypeUint32, Value: 0x50},
+		opcode.Post{},
+
+		opcode.PushI{DataType: vm.TypeVolatilePointer, Value: 1},
+		opcode.Extend{Value: 0x50607},
+		opcode.PushI{DataType: vm.TypeUint32, Value: 2},
+		opcode.Extend{Value: 0x90a0b},
+		opcode.Post{},
+	)
+}
