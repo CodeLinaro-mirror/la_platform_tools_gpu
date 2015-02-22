5f12411bbcc72fd85491cdfb7a7c000f346c7026
Ben Clayton <bclayton@google.com>
2015-02-22 18:45:33 +0000

    Add a helper Transforms.AddFunc.
    This adds a transform from a function, which avoids creating a new type.
    
    Change-Id: I51f68eca704a0b1efecfb1f3478af92f7c2402f6

--- atom/transforms.go
+++ atom/transforms.go
@@ -19,6 +19,20 @@ func (l *Transforms) Add(t ...Transformer) {
 	*l = append(*l, t...)
 }
 
+// Add is a convenience function for appending the list of Transformer functions
+// f to the end of the Transforms list.
+func (l *Transforms) AddFunc(f ...func(id Id, atom Atom, output Writer)) {
+	for _, f := range f {
+		*l = append(*l, transformerFunc(f))
+	}
+}
+
+type transformerFunc func(id Id, atom Atom, output Writer)
+
+func (f transformerFunc) Transform(id Id, atom Atom, output Writer) {
+	f(id, atom, output)
+}
+
 type processWriter struct {
 	t Transformer
 	o Writer
