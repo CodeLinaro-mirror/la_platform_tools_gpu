71d819b75e0f3933d818942152ca4ee04f3db75b
Seb Noury <snoury@google.com>
2015-02-24 15:23:43 +0000

    Update loader.Config to match https://go-review.googlesource.com/#/c/5653/ changes.
    Change-Id: Ic545ed017e37d2e5bb1b7cfa9032144c693e85ca

--- binary/codergen/main.go
+++ binary/codergen/main.go
@@ -56,7 +56,7 @@ func filterStructs(pkg *loader.PackageInfo) []*types.TypeName {
 
 func run() error {
 	flag.Parse()
-	config := loader.Config{SourceImports: true}
+	config := loader.Config{}
 	_, err := config.FromArgs(flag.Args(), false)
 	if err != nil {
 		return err
