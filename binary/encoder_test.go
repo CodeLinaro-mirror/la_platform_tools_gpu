9cce4306aacf17813fc7ca5c64ccd3e89fba746e
Ian Cottrell <iancottrell@google.com>
2015-03-03 00:43:43 +0000

    Adding round trip tests to binary package
    Change-Id: I58b043753e8e340ec9495290ac05b735743ff6da

--- binary/encoder_test.go
+++ binary/encoder_test.go
@@ -70,15 +70,20 @@ func TestEncoderTestError(t *testing.T) {
 	checkEncoderGivesError(t, e, testError)
 }
 
+func compareBytes(t *testing.T, got []byte, expected []byte) {
+	if !bytes.Equal(expected, got) {
+		t.Errorf(`Encode gave unexpected bytes.
+Expected: %# x
+Got:      %# x`, expected, got)
+	}
+}
+
 func TestEncoderBool(t *testing.T) {
 	b := &bytes.Buffer{}
 	e := NewEncoder(b)
 	e.Bool(false)
 	e.Bool(true)
-	expected, got := []byte{0, 1}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	compareBytes(t, b.Bytes(), []byte{0, 1})
 }
 
 func TestEncoderInt8(t *testing.T) {
@@ -88,10 +93,7 @@ func TestEncoderInt8(t *testing.T) {
 	e.Int8(127)
 	e.Int8(-128)
 	e.Int8(-1)
-	expected, got := []byte{0x00, 0x7f, 0x80, 0xff}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	compareBytes(t, b.Bytes(), []byte{0x00, 0x7f, 0x80, 0xff})
 }
 
 func TestEncoderUint8(t *testing.T) {
@@ -101,10 +103,7 @@ func TestEncoderUint8(t *testing.T) {
 	e.Uint8(0x7f)
 	e.Uint8(0x80)
 	e.Uint8(0xff)
-	expected, got := []byte{0x00, 0x7f, 0x80, 0xff}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	compareBytes(t, b.Bytes(), []byte{0x00, 0x7f, 0x80, 0xff})
 }
 
 func TestEncoderInt16(t *testing.T) {
@@ -114,15 +113,12 @@ func TestEncoderInt16(t *testing.T) {
 	e.Int16(32767)
 	e.Int16(-32768)
 	e.Int16(-1)
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		0x00, 0x00,
 		0xff, 0x7f,
 		0x00, 0x80,
 		0xff, 0xff,
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderUint16(t *testing.T) {
@@ -131,14 +127,11 @@ func TestEncoderUint16(t *testing.T) {
 	e.Uint16(0)
 	e.Uint16(0xbeef)
 	e.Uint16(0xc0de)
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		0x00, 0x00,
 		0xef, 0xbe,
 		0xde, 0xc0,
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderInt32(t *testing.T) {
@@ -148,15 +141,12 @@ func TestEncoderInt32(t *testing.T) {
 	e.Int32(2147483647)
 	e.Int32(-2147483648)
 	e.Int32(-1)
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		0x00, 0x00, 0x00, 0x00,
 		0xff, 0xff, 0xff, 0x7f,
 		0x00, 0x00, 0x00, 0x80,
 		0xff, 0xff, 0xff, 0xff,
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderUint32(t *testing.T) {
@@ -165,14 +155,11 @@ func TestEncoderUint32(t *testing.T) {
 	e.Uint32(0)
 	e.Uint32(0x01234567)
 	e.Uint32(0x10abcdef)
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		0x00, 0x00, 0x00, 0x00,
 		0x67, 0x45, 0x23, 0x01,
 		0xef, 0xcd, 0xab, 0x10,
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderFloat32(t *testing.T) {
@@ -181,14 +168,11 @@ func TestEncoderFloat32(t *testing.T) {
 	e.Float32(0)
 	e.Float32(1)
 	e.Float32(64.5)
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x80, 0x3f,
 		0x00, 0x00, 0x81, 0x42,
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderInt64(t *testing.T) {
@@ -198,15 +182,12 @@ func TestEncoderInt64(t *testing.T) {
 	e.Int64(9223372036854775807)
 	e.Int64(-9223372036854775808)
 	e.Int64(-1)
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80,
 		0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderUint64(t *testing.T) {
@@ -215,14 +196,11 @@ func TestEncoderUint64(t *testing.T) {
 	e.Uint64(0)
 	e.Uint64(0x0123456789abcdef)
 	e.Uint64(0xfedcba9876543210)
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0xef, 0xcd, 0xab, 0x89, 0x67, 0x45, 0x23, 0x01,
 		0x10, 0x32, 0x54, 0x76, 0x98, 0xba, 0xdc, 0xfe,
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderFloat64(t *testing.T) {
@@ -232,14 +210,11 @@ func TestEncoderFloat64(t *testing.T) {
 	e.Float64(1)
 	e.Float64(64.5)
 
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x3f,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x50, 0x40,
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderString(t *testing.T) {
@@ -250,7 +225,7 @@ func TestEncoderString(t *testing.T) {
 	e.String("World")
 	e.String("こんにちは世界")
 
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		0x05, 0x00, 0x00, 0x00,
 		'H', 'e', 'l', 'l', 'o',
 		0x00, 0x00, 0x00, 0x00,
@@ -258,10 +233,7 @@ func TestEncoderString(t *testing.T) {
 		'W', 'o', 'r', 'l', 'd',
 		0x15, 0x00, 0x00, 0x00,
 		0xe3, 0x81, 0x93, 0xe3, 0x82, 0x93, 0xe3, 0x81, 0xab, 0xe3, 0x81, 0xa1, 0xe3, 0x81, 0xaf, 0xe4, 0xb8, 0x96, 0xe7, 0x95, 0x8c,
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderCString(t *testing.T) {
@@ -272,15 +244,12 @@ func TestEncoderCString(t *testing.T) {
 	e.CString("World")
 	e.CString("こんにちは世界")
 
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		'H', 'e', 'l', 'l', 'o', 0x00,
 		0x00,
 		'W', 'o', 'r', 'l', 'd', 0x00,
 		0xe3, 0x81, 0x93, 0xe3, 0x82, 0x93, 0xe3, 0x81, 0xab, 0xe3, 0x81, 0xa1, 0xe3, 0x81, 0xaf, 0xe4, 0xb8, 0x96, 0xe7, 0x95, 0x8c, 0x00,
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderObject(t *testing.T) {
@@ -298,7 +267,7 @@ func TestEncoderObject(t *testing.T) {
 	if err := e.Object(nil); err != nil {
 		t.Errorf("Encode gave unexpected error: %v", err)
 	}
-	expected, got := []byte{
+	compareBytes(t, b.Bytes(), []byte{
 		0x00, 0x00,
 		0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x07, 0x00, 0x00, 0x00,
@@ -312,10 +281,7 @@ func TestEncoderObject(t *testing.T) {
 		0x00, 0x00,
 
 		byte(objectNil & 0xff), byte((objectNil >> 8) & 0xff),
-	}, b.Bytes()
-	if !bytes.Equal(expected, got) {
-		t.Errorf("Encode gave unexpected bytes. Expected: %v, got: %v", expected, got)
-	}
+	})
 }
 
 func TestEncoderObjectUnknownTypeError(t *testing.T) {
