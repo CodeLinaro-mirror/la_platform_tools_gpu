c27df932ca1efe0ee6036808775000993bd34832
Brent Austin <baustin@google.com>
2015-03-18 10:17:43 -0700

    Added option to cross-compile for windows (on by default).
    Currently only builds gaze.exe for windows.
    Also some cleanup and refactoring.
    
    Change-Id: Ib033e080c3cd8bb078fccd60d5da2b157a73d88e

--- tools/make_gpu_tools.sh
+++ tools/make_gpu_tools.sh
@@ -7,27 +7,32 @@ set -ex
 PROGDIR=`dirname $0`
 PROGDIR=`cd $PROGDIR && pwd`
 
-source $PROGDIR/setup_env_linux.txt
+source $PROGDIR/setup_env_common.txt
+source $PROGDIR/setup_toolchain_linux64.txt
 
+crosscompile_windows=1
 run_integration_tests=0
 use_xvfb=0
 
 function show_help {
   # Turn off command echoing so the help message is readable.
   set +x
-  echo "USAGE: "`basename $0`" [-h] [-x] [-i]"
+  echo "USAGE: "`basename $0`" [-h] [-i] [-w] [-x]"
   echo "  -h    Show this message."
   echo "  -i    Run integration tests."
+  echo "  -w    Do NOT cross-compile the server for Windows."
   echo "  -x    Start an Xvfb-randr server for running integration tests"
   echo "        without an X server."
 }
 
-while getopts "h?ix" opt; do
+while getopts "h?iwx" opt; do
     case "$opt" in
     h|\?)  show_help
         exit 0
         ;;
     i)  run_integration_tests=1
+        ;;
+    w)  crosscompile_windows=0
         ;;
     x)  use_xvfb=1
         ;;
@@ -47,36 +52,40 @@ if [ $use_xvfb -eq 1 ]; then
   export DISPLAY=:42
 fi
 
-go build -i -o $GPU_BUILD_ROOT/bin/embed $GPU_RELATIVE_SOURCE_PATH/tools/embed
-go generate -x $GPU_RELATIVE_SOURCE_PATH/tools/copyright
-go generate -x $GPU_RELATIVE_SOURCE_PATH/binary/generate
-go generate -x $GPU_RELATIVE_SOURCE_PATH/rpc/generate
-go build -i -o $GPU_BUILD_ROOT/bin/codergen $GPU_RELATIVE_SOURCE_PATH/binary/codergen
-go generate -x $GPU_RELATIVE_SOURCE_PATH/replay/protocol
-go generate -x $GPU_RELATIVE_SOURCE_PATH/rpc
-go build -i -o $GPU_BUILD_ROOT/bin/rpcapi $GPU_RELATIVE_SOURCE_PATH/rpc/rpcapi
-go generate -x $GPU_RELATIVE_SOURCE_PATH/rpc/test
-go generate -x $GPU_RELATIVE_SOURCE_PATH/service
-go build -i -o $GPU_BUILD_ROOT/bin/apic $GPU_RELATIVE_SOURCE_PATH/api/apic
-go generate -x $GPU_RELATIVE_SOURCE_PATH/gfxapi/test
-go generate -x $GPU_RELATIVE_SOURCE_PATH/gfxapi/gles
-go generate -x $GPU_RELATIVE_SOURCE_PATH/atom
-go generate -x $GPU_RELATIVE_SOURCE_PATH/builder
-go generate -x $GPU_RELATIVE_SOURCE_PATH/database
-go generate -x $GPU_RELATIVE_SOURCE_PATH/memory
-go build -i -o $GPU_BUILD_ROOT/bin/gazer $GPU_RELATIVE_SOURCE_PATH/server/cmd
+export GO_BUILD_FLAGS="-i -v -x -o"
+export GO_GENERATE_FLAGS="-v -x"
+export GO_TEST_FLAGS="-v -x"
 
-src/$GPU_RELATIVE_SOURCE_PATH/cc/gradlew -b src/$GPU_RELATIVE_SOURCE_PATH/cc/build.gradle
+go build    $GO_BUILD_FLAGS    $GPU_BUILD_ROOT/bin/embed $GPU_RELATIVE_SOURCE_PATH/tools/embed
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/tools/copyright
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/binary/generate
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/rpc/generate
+go build    $GO_BUILD_FLAGS    $GPU_BUILD_ROOT/bin/codergen $GPU_RELATIVE_SOURCE_PATH/binary/codergen
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/replay/protocol
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/rpc
+go build    $GO_BUILD_FLAGS    $GPU_BUILD_ROOT/bin/rpcapi $GPU_RELATIVE_SOURCE_PATH/rpc/rpcapi
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/rpc/test
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/service
+go build    $GO_BUILD_FLAGS    $GPU_BUILD_ROOT/bin/apic $GPU_RELATIVE_SOURCE_PATH/api/apic
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/gfxapi/test
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/gfxapi/gles
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/atom
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/builder
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/database
+go generate $GO_GENERATE_FLAGS $GPU_RELATIVE_SOURCE_PATH/memory
+go build    $GO_BUILD_FLAGS    $GPU_BUILD_ROOT/bin/gazer $GPU_RELATIVE_SOURCE_PATH/server/cmd
+
+src/$GPU_RELATIVE_SOURCE_PATH/cc/gradlew -b src/$GPU_RELATIVE_SOURCE_PATH/cc/build.gradle --info
 
 # Kill any existing replay daemon before running tests.
 killall replayd || true
 
 # Run non-integration tests.
-go list android.googlesource.com/platform/tools/gpu/... | egrep -v '/integration/?' | xargs go test
+go list android.googlesource.com/platform/tools/gpu/... | egrep -v '/integration/?' | xargs go test $GO_TEST_FLAGS
 
 if [ $run_integration_tests -eq 1 ]; then
   # Run the integration tests.
-  go list android.googlesource.com/platform/tools/gpu/... | egrep '/integration/?' | xargs go test
+  go list android.googlesource.com/platform/tools/gpu/... | egrep '/integration/?' | xargs go test $GO_TEST_FLAGS
 fi
 
 if [ ! -z $XVFB_PID ]; then
@@ -84,3 +93,9 @@ if [ ! -z $XVFB_PID ]; then
 fi
 
 killall replayd || true
+
+if [ $crosscompile_windows -eq 1 ]; then
+  source $PROGDIR/setup_toolchain_linux_xc_win64.txt
+  go build $GO_BUILD_FLAGS $GPU_BUILD_ROOT/bin/windows_amd64/gazer.exe -ldflags="-extld=$CC" $GPU_RELATIVE_SOURCE_PATH/server/cmd
+  # TODO: Use gradle to build replayd.exe for windows.
+fi
