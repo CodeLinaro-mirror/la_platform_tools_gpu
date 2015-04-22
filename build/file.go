e2a745b6367fb40ac68896f84482760702c00949
Ben Clayton <bclayton@google.com>
2015-04-22 16:14:12 +0100

    Don't use os.File.Chmod - it errors on Windows
    Change-Id: I887b4cc8013d3d1c951c6889c1753a9a757005fc

--- build/file.go
+++ build/file.go
@@ -163,15 +163,11 @@ func (f File) CopyTo(dst File) error {
 		return err
 	}
 
-	d, err := os.Create(dst.Absolute())
+	d, err := os.OpenFile(dst.Absolute(), os.O_CREATE, fi.Mode())
 	if err != nil {
 		return err
 	}
 	defer d.Close()
-
-	if err := d.Chmod(fi.Mode()); err != nil {
-		return err
-	}
 
 	_, err = io.Copy(d, s)
 	return err
