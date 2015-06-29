f913ac5408f44dbf914cc659b430c898bf6123ad
Ian Cottrell <iancottrell@google.com>
2015-06-29 19:11:03 +0100

    Add atom metadata to schema
    Change-Id: I45ecb4979d8f340eb0effef2d8b145d2ff4eff32

---
+++ atom/schema.go
@@ -0,0 +1,26 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package atom
+
+import "android.googlesource.com/platform/tools/gpu/binary"
+
+// Metadata is the meta information about an atom type that is added to the
+// binary schema class for the atom.
+type Metadata struct {
+	binary.Generate
+	Api              binary.ID // The api this atom belongs to.
+	Flags            Flags     // The atom flags for this type.
+	DocumentationUrl string    // A url for documentation about this atom.
+}
