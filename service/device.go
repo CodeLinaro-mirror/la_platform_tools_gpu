eb0072f7052b8cb15661d4b4fcd645c94109b231
Ian Cottrell <iancottrell@google.com>
2015-07-16 14:00:36 +0100

    Move device stuff to device.go
    Change-Id: I28c32e1edeb777588ce0990fee808554b6ed9190

--- service/device.go
+++ service/device.go
@@ -18,9 +18,27 @@ import (
 	"fmt"
 	"regexp"
 
+	"android.googlesource.com/platform/tools/gpu/binary"
 	"android.googlesource.com/platform/tools/gpu/binary/endian"
 	"android.googlesource.com/platform/tools/gpu/device"
 )
+
+type DeviceID binary.ID
+
+// Device describes replay target avaliable to the server.
+type Device struct {
+	binary.Generate  `handle:"DeviceID"`
+	Name             string // The name of the device. e.g. "Bob's phone"
+	Model            string // The model of the device. e.g. "Nexus 5"
+	OS               string // The operating system of the device. e.g. "Android 5.0"
+	PointerSize      uint8  // Size in bytes of a pointer on the device's architecture.
+	PointerAlignment uint8  // Alignment in bytes of a pointer on the device's architecture.
+	MaxMemorySize    uint64 // The total amount of contiguous memory pre-allocated for replay.
+	Extensions       string // Renderer extensions list. e.g. "GL_KHR_debug GL_EXT_sRGB [...]".
+	Renderer         string // Renderer name. e.g. "Adreno (TM) 320".
+	Vendor           string // Renderer vendor name. e.g. "Qualcomm".
+	Version          string // Renderer version. e.g. "OpenGL ES 3.0 V@53.0 AU@  (CL@)".
+}
 
 // Architecture return's the device's architecture.
 func (d Device) Architecture() device.Architecture {
