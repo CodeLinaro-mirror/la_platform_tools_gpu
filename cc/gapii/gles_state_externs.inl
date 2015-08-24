6e24a563b8e0a258d6b603fdd0ad8501efa98658
Ben Clayton <bclayton@google.com>
2015-08-24 16:32:13 +0100

    Add mapMemory and unmapMemory externs.
    These pass through to replay.Builder.MapMemory and UnmapMemory.
    They may want to become first-class intrinsics at some point, but 
    figuring out where the replay pointer value is stored might be tricky.
    For now hand-craft the MapMemory function.
    
    Change-Id: I851a83a7ff339a4db70fc58bb2adb1104851def1

--- cc/gapii/gles_state_externs.inl
+++ cc/gapii/gles_state_externs.inl
@@ -17,6 +17,9 @@
 template<typename T> T inline min(T a, T b) { return (a < b) ? a : b; }
 template<typename T> T inline max(T a, T b) { return (a > b) ? a : b; }
 
+template<typename T> inline void mapMemory(const T&) {}
+template<typename T> inline void unmapMemory(const T&) {}
+
 inline uint32_t minIndex(const uint8_t* indices, uint32_t indices_type, uint32_t offset, uint32_t count) {
     uint32_t v = ~(uint32_t)0;
     switch (indices_type) {
--- gfxapi/gles/externs.go
+++ gfxapi/gles/externs.go
@@ -34,6 +34,25 @@ type externs struct {
 	b *rb.Builder
 }
 
+func (e externs) mapMemory(slice Slice) {
+	if b := e.b; b != nil {
+		switch e.a.(type) {
+		case *GlMapBufferRange:
+			// Base address is on the stack.
+			b.MapMemory(slice.Range(e.s))
+
+		default:
+			log.E(e.l, "mapBuffer extern called for unsupported atom %T", e.a)
+		}
+	}
+}
+
+func (e externs) unmapMemory(slice Slice) {
+	if b := e.b; b != nil {
+		b.UnmapMemory(slice.Range(e.s))
+	}
+}
+
 func (e externs) elSize(ty GLenum) uint64 {
 	switch ty {
 	case GLenum_GL_UNSIGNED_BYTE:
