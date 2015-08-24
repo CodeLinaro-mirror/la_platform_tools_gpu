61308d9c4ea440df343b32ad35c44404ca3a7194
Ben Clayton <bclayton@google.com>
2015-08-24 16:34:09 +0100

    Remove the panic handling from the replay batcher.
    It causes more issues than it solves.
    If we’re panicking, we need to fix it.
    
    Change-Id: I98e4ae0c5d694831798db0201aecabd428c807bd

--- replay/batcher.go
+++ replay/batcher.go
@@ -85,31 +85,26 @@ func (b *batcher) send(requests []Request) (err error) {
 
 	builder := builder.New(architecture)
 
-	if err := func() (err interface{}) {
-		// Prevent panics from causing GAPIS to fall over.
-		// This is temporary, as state mutators should return errors instead of
-		// causing runtime panics.
-		defer func() { err = recover() }()
+	out := &adapter{
+		state:   gfxapi.NewState(),
+		db:      b.database,
+		logger:  b.logger,
+		builder: builder,
+	}
 
-		out := &adapter{
-			state:   gfxapi.NewState(),
-			db:      b.database,
-			logger:  b.logger,
-			builder: builder,
-		}
+	if err := b.context.Generator.Replay(
+		b.context.Context,
+		b.context.Config,
+		requests,
+		device,
+		*list,
+		out,
+		b.database,
+		b.logger); err != nil {
 
-		return b.context.Generator.Replay(
-			b.context.Context,
-			b.context.Config,
-			requests,
-			device,
-			*list,
-			out,
-			b.database,
-			b.logger)
-	}(); err != nil {
-		log.Errorf(b.logger, "Panic raised while writing atoms for replay: %v", err)
-		return fmt.Errorf("%v", err)
+		// TODO: Inform the request that the replay failed.
+		log.Errorf(b.logger, "Replay returned error: %v", err)
+		return err
 	}
 
 	if config.DebugReplay {
