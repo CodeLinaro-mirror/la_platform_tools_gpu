7114f25247edfc352975c3b571754bac72d05517
Pascal Muetschard <pmuetschard@google.com>
2015-09-17 15:49:18 -0700

    Fix the linux/windows build.
    Change-Id: I736a01bf5e74312f6652869420b90455134541fe

--- tools/make_gpu_tools.sh
+++ tools/make_gpu_tools.sh
@@ -104,7 +104,7 @@ fi
 killall gapir || true
 
 if [ $crosscompile_windows -eq 1 ]; then
-  go run src/$GPU_RELATIVE_SOURCE_PATH/make.go -f -v=1 -verbose=true -targetos=windows --disable=code cc:replayd
+  go run src/$GPU_RELATIVE_SOURCE_PATH/make.go -f -v=1 -verbose=true -targetos=windows --disable=code cc:gapir
   source $PROGDIR/setup_toolchain_linux_xc_win64.txt
   go build $GO_BUILD_FLAGS $GPU_BUILD_ROOT/bin/windows-x64/$BUILD_FLAVOR/gapis.exe -ldflags="-extld=$CC" $GPU_RELATIVE_SOURCE_PATH/server/gapis
 fi
