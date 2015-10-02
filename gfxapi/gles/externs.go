7f73a5c02e505b03343fc50540555922dd204aa8
David Srbecky <dsrbecky@google.com>
2015-10-02 16:51:31 +0100

    Report the size of GL_MAX_SAMPLES and GL_NUM_PROGRAM_BINARY_FORMATS
    I missed those in the last CL
    
    Change-Id: Ie6b4ebc62b7fad40ae5472e446046240e4ac4c9a

--- gfxapi/gles/externs.go
+++ gfxapi/gles/externs.go
@@ -204,6 +204,10 @@ func (e externs) stateVariableSize(v GLenum) int32 {
 		return 1
 	case GLenum_GL_SAMPLES:
 		return 1
+	case GLenum_GL_MAX_SAMPLES:
+		return 1
+	case GLenum_GL_NUM_PROGRAM_BINARY_FORMATS:
+		return 1
 	case GLenum_GL_SCISSOR_BOX:
 		return 4
 	case GLenum_GL_SCISSOR_TEST:
