b25e7e535182fc7556548a50d56243b3bda3e3d2
Ben Clayton <bclayton@google.com>
2015-10-11 21:03:49 +0100

    Complement glsl/ast.VisitChildren with TransformChildren.
    This can be used to replace AST nodes.
    
    Change-Id: Id1bb444ed6e58e7765b22d1aeaad06f52d490ceb

--- gfxapi/gles/glsl/ast/visitor.go
+++ gfxapi/gles/glsl/ast/visitor.go
@@ -17,145 +17,168 @@ package ast
 // ChildVisitor is a callback function used by VisitChildren to visit nodes.
 type ChildVisitor func(interface{})
 
-// VisitChildren is a helper function which calls the provided callback for each child node of
-// the argument.
-func VisitChildren(n interface{}, v ChildVisitor) {
+// ChildTransformer is a callback function used by TransformChildren to visit nodes.
+type ChildTransformer func(child, parent interface{}) interface{}
+
+// TransformChildren is a helper function which calls the provided callback for
+// each child node of the argument, replacing their values with the values
+// returned from v.
+func TransformChildren(n interface{}, v ChildTransformer) {
 	switch n := n.(type) {
 	case *BinaryExpr:
-		v(n.Left)
-		v(n.Right)
+		n.Left = v(n.Left, n).(Expression)
+		n.Right = v(n.Right, n).(Expression)
 	case *IndexExpr:
-		v(n.Base)
-		v(n.Index)
+		n.Base = v(n.Base, n).(Expression)
+		n.Index = v(n.Index, n).(Expression)
 	case *ConditionalExpr:
-		v(n.Cond)
-		v(n.TrueExpr)
-		v(n.FalseExpr)
+		n.Cond = v(n.Cond, n).(Expression)
+		n.TrueExpr = v(n.TrueExpr, n).(Expression)
+		n.FalseExpr = v(n.FalseExpr, n).(Expression)
 	case *UnaryExpr:
-		v(n.Expr)
+		n.Expr = v(n.Expr, n).(Expression)
 	case *DotExpr:
-		v(n.Expr)
-	case *ConstantExpr, *VarRefExpr:
+		n.Expr = v(n.Expr, n).(Expression)
+	case *ConstantExpr:
+		n.Value = v(n.Value, n).(Value)
+	case *VarRefExpr:
+		n.Sym = v(n.Sym, n).(ValueSymbol)
 	case *TypeConversionExpr:
-		v(n.RetType)
-	case *ParenExpr:
-		v(n.Expr)
-	case *CallExpr:
-		v(n.Callee)
-		for _, a := range n.Args {
-			v(a)
+		if n.RetType != nil {
+			n.RetType = v(n.RetType, n).(Type)
 		}
-
+	case *ParenExpr:
+		n.Expr = v(n.Expr, n).(Expression)
+	case *CallExpr:
+		n.Callee = v(n.Callee, n).(Expression)
+		for i, c := range n.Args {
+			n.Args[i] = v(c, n).(Expression)
+		}
 	case *DeclarationStmt:
-		v(n.Decl)
+		n.Decl = v(n.Decl, n)
 	case *ExpressionStmt:
-		v(n.Expr)
+		n.Expr = v(n.Expr, n).(Expression)
 	case *CaseStmt:
-		v(n.Expr)
+		n.Expr = v(n.Expr, n).(Expression)
 	case *SwitchStmt:
-		v(n.Expr)
-		v(n.Stmts)
+		n.Expr = v(n.Expr, n).(Expression)
+		n.Stmts = v(n.Stmts, n).(*CompoundStmt)
 	case *WhileStmt:
-		v(n.Cond)
-		v(n.Stmt)
+		n.Cond = v(n.Cond, n)
+		n.Stmt = v(n.Stmt, n)
 	case *DoStmt:
-		v(n.Stmt)
-		v(n.Expr)
+		n.Stmt = v(n.Stmt, n)
+		n.Expr = v(n.Expr, n).(Expression)
 	case *ForStmt:
-		v(n.Init)
-		v(n.Cond)
-		v(n.Loop)
-		v(n.Body)
+		n.Init = v(n.Init, n)
+		n.Cond = v(n.Cond, n)
+		n.Loop = v(n.Loop, n).(Expression)
+		n.Body = v(n.Body, n)
 	case *ContinueStmt, *BreakStmt, *DiscardStmt, *EmptyStmt, *DefaultStmt:
 	case *IfStmt:
-		v(n.IfExpr)
-		v(n.ThenStmt)
+		n.IfExpr = v(n.IfExpr, n).(Expression)
+		n.ThenStmt = v(n.ThenStmt, n)
 		if n.ElseStmt != nil {
-			v(n.ElseStmt)
+			n.ElseStmt = v(n.ElseStmt, n)
 		}
 	case *CompoundStmt:
-		for _, s := range n.Stmts {
-			v(s)
+		for i, c := range n.Stmts {
+			n.Stmts[i] = v(c, n)
 		}
 	case *ReturnStmt:
 		if n.Expr != nil {
-			v(n.Expr)
+			n.Expr = v(n.Expr, n).(Expression)
 		}
-
 	case *BuiltinType:
 	case *ArrayType:
-		v(n.Base)
+		if n.Base != nil {
+			n.Base = v(n.Base, n).(Type)
+		}
 		if n.Size != nil {
-			v(n.Size)
+			n.Size = v(n.Size, n).(Expression)
 		}
 	case *StructType:
 		if n.StructDef {
-			v(n.Sym)
+			n.Sym = v(n.Sym, n).(*StructSym)
 		}
-
 	case *LayoutQualifier:
 	case *TypeQualifiers:
 		if n.Layout != nil {
-			v(n.Layout)
+			n.Layout = v(n.Layout, n).(*LayoutQualifier)
 		}
-
 	case *PrecisionDecl:
-		v(n.Type)
+		if n.Type != nil {
+			n.Type = v(n.Type, n).(*BuiltinType)
+		}
 	case *FunctionDecl:
-		v(n.RetType)
-		for _, p := range n.Params {
-			v(p)
+		if n.RetType != nil {
+			n.RetType = v(n.RetType, n).(Type)
+		}
+		for i, c := range n.Params {
+			n.Params[i] = v(c, n).(*FuncParameterSym)
 		}
 		if n.Stmts != nil {
-			v(n.Stmts)
+			n.Stmts = v(n.Stmts, n).(*CompoundStmt)
 		}
 	case *MultiVarDecl:
 		if n.Quals != nil {
-			v(n.Quals)
+			n.Quals = v(n.Quals, n).(*TypeQualifiers)
 		}
-		v(n.Type)
-		for _, n := range n.Vars {
-			v(n)
+		if n.Type != nil {
+			n.Type = v(n.Type, n).(Type)
+		}
+		for i, c := range n.Vars {
+			n.Vars[i] = v(c, n).(*VariableSym)
 		}
 	case *LayoutDecl:
-		v(n.Layout)
+		n.Layout = v(n.Layout, n).(*LayoutQualifier)
 	case *InvariantDecl:
-		for _, n := range n.Vars {
-			v(n)
+		for i, c := range n.Vars {
+			n.Vars[i] = v(c, n).(*VarRefExpr)
 		}
 	case *UniformDecl:
-		v(n.Block)
+		n.Block = v(n.Block, n).(*UniformBlock)
 		if n.Size != nil {
-			v(n.Size)
+			n.Size = v(n.Size, n).(Expression)
 		}
-
 	case *VariableSym:
-		v(n.Type)
+		if n.SymType != nil {
+			n.SymType = v(n.SymType, n).(Type)
+		}
 		if n.Init != nil {
-			v(n.Init)
+			n.Init = v(n.Init, n).(Expression)
 		}
 	case *FuncParameterSym:
-		v(n.SymType)
+		if n.SymType != nil {
+			n.SymType = v(n.SymType, n).(Type)
+		}
 	case *StructSym:
-		for _, n := range n.Vars {
-			v(n)
+		for i, c := range n.Vars {
+			n.Vars[i] = v(c, n).(*MultiVarDecl)
 		}
 	case *UniformBlock:
 		if n.Layout != nil {
-			v(n.Layout)
+			n.Layout = v(n.Layout, n).(*LayoutQualifier)
 		}
-		for _, n := range n.Vars {
-			v(n)
+		for i, c := range n.Vars {
+			n.Vars[i] = v(c, n).(*MultiVarDecl)
 		}
-
 	case *ExpressionCond:
-		v(n.Expr)
+		n.Expr = v(n.Expr, n).(Expression)
 	case *VarDeclCond:
-		v(n.Sym)
-
+		n.Sym = v(n.Sym, n).(*VariableSym)
 	case *Ast:
-		for _, n := range n.Decls {
-			v(n)
+		for i, c := range n.Decls {
+			n.Decls[i] = v(c, n)
 		}
 	}
+}
+
+// VisitChildren is a helper function which calls the provided callback for each child node of
+// the argument.
+func VisitChildren(n interface{}, v ChildVisitor) {
+	TransformChildren(n, func(child, parent interface{}) interface{} {
+		v(child)
+		return child
+	})
 }
